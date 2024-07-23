Fix causal mask (use bool) and implement generation util

cronformer/modeling_cronformer.py
@@ -29,12 +29,13 @@ class DecoderLayer(nn.Module):
 
     def forward(self, hidden_states, encoder_outputs, attention_mask):
         hidden_states = self.layer_norm1(hidden_states)
-        self_attn_output, _ = self.self_attn(hidden_states, hidden_states, hidden_states, attn_mask=attention_mask)
+        self_attn_causal_mask = torch.triu(torch.ones(hidden_states.size(1), hidden_states.size(1)), diagonal=1).to(hidden_states.device).bool()
+        self_attn_output, _ = self.self_attn(hidden_states, hidden_states, hidden_states, attn_mask=self_attn_causal_mask, is_causal=True)
         hidden_states = hidden_states + self.dropout(self_attn_output)
 
         hidden_states = self.layer_norm2(hidden_states)
 
-        cross_attn_output, _ = self.cross_attn(hidden_states, encoder_outputs, encoder_outputs)
+        cross_attn_output, _ = self.cross_attn(hidden_states, encoder_outputs, encoder_outputs, attn_mask=attention_mask)
         hidden_states = hidden_states + self.dropout(cross_attn_output)
 
         hidden_states = self.layer_norm3(hidden_states)
@@ -51,17 +52,32 @@ class CronDecoder(nn.Module):
     def __init__(self, config: BertConfig):
         super(CronDecoder, self).__init__()
 
+        num_decoder_layers = 2
+
+        self.config = config
         self.token_embedding = nn.Embedding(output_vocab_size, config.hidden_size)
-        self.decoder_layer = DecoderLayer(config)
+        self.decoder_layers = nn.ModuleList([
+            DecoderLayer(config)
+            for _ in range(num_decoder_layers)
+        ])
         self.cron_heads = nn.ModuleList([
             nn.Linear(config.hidden_size, output_vocab_size)
             for _ in range(CRON_DIMS)
         ])
 
-    def forward(self, output_ids, encoder_outputs, cron_dim):
+    def forward(self, output_ids, encoder_outputs, cron_dim, attention_mask):
         embeddings = self.token_embedding(output_ids)
-        attention_mask = torch.triu(torch.ones(output_ids.size(1), output_ids.size(1)), diagonal=1).to(output_ids.device)
-        hidden_states = self.decoder_layer(embeddings, encoder_outputs, attention_mask=attention_mask)
+
+        if attention_mask is not None and len(attention_mask.shape) == 2:
+            attention_mask = (attention_mask.unsqueeze(1)
+                              .repeat_interleave(self.config.num_attention_heads, dim=0)
+                              .repeat_interleave(output_ids.size(1), dim=1)
+                              .bool())
+
+        hidden_states = embeddings
+        for decoder_layer in self.decoder_layers:
+            hidden_states = decoder_layer(hidden_states, encoder_outputs, attention_mask=attention_mask)
+
         return self.cron_heads[cron_dim](hidden_states)
 
 
@@ -79,7 +95,7 @@ class CronformerModel(PreTrainedModel):
 
         encoder_outputs = self.encoder(input_ids, attention_mask).last_hidden_state
         logits = torch.stack(
-            [self.decoder(output_ids[i], encoder_outputs, i) for i in cron_dims],
+            [self.decoder(output_ids[i], encoder_outputs, i, attention_mask) for i in cron_dims],
             dim=0,
         )
 

cronformer/train_cronformer.py
@@ -37,7 +37,7 @@ class CronExpressionDataset(Dataset):
         input_text, output_cron = self.data[idx]
         tokenized = self.input_tokenizer(input_text, max_length=self.input_sequence_length, padding="max_length", return_tensors="pt")
         input_ids = tokenized["input_ids"].squeeze(0)
-        attention_mask = tokenized["attention_mask"].squeeze(0)
+        attention_mask = tokenized["attention_mask"].squeeze(0).bool()
         output_ids = torch.tensor(self.output_tokenizer.tokenize(output_cron, sequence_length=self.sequence_length))
         return input_ids, attention_mask, output_ids
 
@@ -82,6 +82,7 @@ if __name__ == "__main__":
     parser.add_argument("-o", "--output-dir", type=str, default="./.cronformer", help="Output directory for the model")
     parser.add_argument("--input-sequence-length", type=int, default=512, help="Maximum sequence length for the input")
     parser.add_argument("-s", "--sequence-length", type=int, default=128, help="Maximum sequence length for the model")
+    parser.add_argument("--max-steps", type=int, default=0, help="Maximum number of steps to train")
     parser.add_argument("--validation-split", type=float, default=0.1, help="Fraction of the dataset to use for validation")
     parser.add_argument("--validation-stride", type=int, default=1000, help="Number of steps between validation runs")
     parser.add_argument("--disable-wandb", action="store_true", help="Disable logging to wandb")
@@ -180,6 +181,10 @@ if __name__ == "__main__":
             print(f"Epoch {epoch}, Step {num_steps}, Loss {batch_loss}")
 
             num_steps += 1
+
+            if args.max_steps > 0 and num_steps >= args.max_steps:
+                print("WARNING: Maximum number of steps reached")
+                break
 
     model.save_pretrained(path.join(output_dir, "final"))
 

tests/test_cronformer.py
@@ -3,6 +3,8 @@ import unittest
 from os import path, environ
 
 import torch
+
+from cronformer.generation import generate
 from cronformer.modeling_cronformer import CronformerModel
 from cronformer.tokenization_cronformer import CronformerTokenizer
 from transformers import BertTokenizer
@@ -30,32 +32,10 @@ class TestCronformer(unittest.TestCase):
         model.to(device)
 
         for input, output in map(lambda entry: (entry["input"], entry["output"]), evals):
-            input_ids = torch.tensor(input_tokenizer.encode(input))[None].to(device)
-            output_ids = torch.tensor(output_tokenizer.tokenize(output)).to(device).unsqueeze(dim=1)
-
-            logits = model(input_ids, output_ids)
-            predicted_ids = torch.argmax(logits, dim=-1).squeeze(dim=1)
-
-            prediction_ids = []
-            prediction_tokens = []
-
-            for i, prediction_component_ids in enumerate(predicted_ids):
-                output_component_ids = output_ids[i].view(-1)
-                try:
-                    pad_token_id_index = output_component_ids.tolist().index(output_tokenizer.output_tokenizer.token_to_id("<pad>"))
-                except ValueError:
-                    pad_token_id_index = len(output_component_ids)
-                prediction_component_ids[pad_token_id_index:] = output_tokenizer.output_tokenizer.token_to_id("<pad>")
-                prediction_ids += [output_component_ids.tolist()[0]] + prediction_component_ids.tolist()[:-1]
-                prediction_tokens += [output_tokenizer.decode(prediction_component_ids.tolist()[0:pad_token_id_index - 1], skip_special_tokens=True)]
-
-            prediction = " ".join(prediction_tokens)
-
-            if environ.get("CRONFORMER_DEBUG") == "1":
-                print(f"[INPUT] {input}")
-                print(f"[LABEL] {output_tokenizer.decode(output_ids.view(-1).tolist(), skip_special_tokens=False).replace('><', '> <').replace('<pad>', '')}")
-                print(f"[PRED.] {prediction}")
-                print()
+            output_ids = torch.tensor(output_tokenizer.tokenize(output))
+            generation = generate(model, input)
+            prediction_ids = generation.token_ids
+            prediction = generation.completion
 
             with self.subTest(input):
                 self.assertEqual(output_ids.view(-1).tolist(), prediction_ids, f"Expected {output} but got {prediction}")